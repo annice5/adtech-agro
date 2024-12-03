9123ab92a78e6197d67c2fe2e4cc1efaed9101ed
Agnes odame <agnesodame5@.com>
2024-12-03 11:55:26 +0000

    changes

--- src/pages/landing/components/footer.jsx
+++ src/pages/landing/components/footer.jsx
@@ -63,7 +63,7 @@ const Footer = () => {
           </div>
 
           {/* Column 3: Contact Information */}
-          <div className="text-center md:text-left">
+          <div className="text-right md:text-left">
             <h3 className="text-xl font-semibold mb-4 text-white">Have a Question?</h3>
             <ul className="space-y-4">
               <li>
